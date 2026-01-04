ab36a5f3cbe13be180a69e9a4715e7f9247bc92b
Raj D <drajesh@hotmail.com>
2026-01-04 02:30:24 -0700

    Update player name layout to line-by-line for better space

--- obj/Debug/net9.0/TournamentScheduler.App.AssemblyInfo.cs
+++ obj/Debug/net9.0/TournamentScheduler.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourneyPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adaf95c53c646f4a04fb2802b4a80e87e17bca17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b5348d3685d5c1f6818623cf9a6c7742a53d3d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourneyPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourneyPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
