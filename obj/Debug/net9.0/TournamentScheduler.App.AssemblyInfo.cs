ac65f4827adb34178b366ce56288939d63297598
Raj D <drajesh@hotmail.com>
2026-01-04 01:48:56 -0700

    Add Current and Next round display to Schedule page

--- obj/Debug/net9.0/TournamentScheduler.App.AssemblyInfo.cs
+++ obj/Debug/net9.0/TournamentScheduler.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TourneyPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dca37c2e5adb01ded5d9ad79aa1c67860664957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+808cc5c8021557b0efa9aac80934c911fb020346")]
 [assembly: System.Reflection.AssemblyProductAttribute("TourneyPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TourneyPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
